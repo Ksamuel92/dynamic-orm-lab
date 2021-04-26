262785dad4fea67f604546ea17af8aeec3b6c326
Kyle Samuel <Ksamuel92@gmail.com>
2021-04-26 12:34:04 -0400

    commit tree b89d3a76753ec26455aa42f02aa6489f49166770

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    self.column_names.each do |col_name| #This simply creates the attr_accessor's for the class by taking the column names and turning them into symbols.
+        attr_accessor col_name.to_sym
+  end
+
 end
